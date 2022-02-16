edb4e3b1feee0d8055ad8f960add0fccf393686c
unknown <zakariae.dinar@gmail.com>
2022-02-16 15:40:03 +0100

    Adding the JUnit test

--- src/test/java/com/example/learning/LearningApplicationTests.java
+++ src/test/java/com/example/learning/LearningApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.learning;
 
+import com.example.learning.repository.EmploeeRepository;
+import com.example.learning.service.EmploeeService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
 class LearningApplicationTests {
 
+	MockMvc mvc;
+	@Autowired
+	WebApplicationContext webApplicationContext;
+	@Autowired
+	EmploeeRepository emploeeRepository;
 	@Test
-	void contextLoads() {
+	void getAllEmploeesTest() throws Exception {
+		String url ="/employees";
+		mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(url)
+				.accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+		int status = mvcResult.getResponse().getStatus();
+		Assertions.assertEquals(200,status);
 	}
 
 }
